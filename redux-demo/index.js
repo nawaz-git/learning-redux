b5db5841a2a1fa52dcdc25595e9c40c43bbb0466
Nawaz Pasha <nawaz22022001@gmail.com>
2023-06-09 11:29:46 +0530

    seperated reducers for each item

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -2,7 +2,7 @@ const redux = require('redux')
 const createStore = redux.createStore
 
 
-// Goal is to Manage Ice Cream aswell now
+// Goal 1.0 is to Manage Ice Cream aswell now
 // 1. Create a Action Type
 const BUY_ICECREAM = 'BUY_ICECREAM'
 // 2. Create Action Creator
@@ -16,6 +16,10 @@ function buyIceCream() {
 // 4. Update Reducer to have Buy IceCream action ✅
 // 5. Buy some IceCreams ✅
 
+// Goal 2.0 Create seperate reducers for each item
+// 1. Seperate States ✅
+// 2. Seperate Reducers ✅
+
 // Create a Action Type
 const BUY_CAKE = 'BUY_CAKE'
 
@@ -28,25 +32,62 @@ function buyCake() {
 }
 
 // Initial State
-const initialState = {
-    numOfCakes: 10,
-    numOfIceCreams: 10
+// const initialState = {
+//     numOfCakes: 10,
+//     numOfIceCreams: 10
+// }
+
+// Cake State
+const cakeState = {
+    numOfCakes: 10
+}
+
+// IceCream State
+const IceCreamState = {
+    numOfIceCream: 10
 }
 
 // Create a Reducer
-const reducer = (state = initialState, action) => {
+// const reducer = (state = initialState, action) => {
+//     switch (action.type) {
+//         case BUY_CAKE: return {
+//             ...state,
+//             numOfCakes: state.numOfCakes - 1
+//         }
+//         case BUY_ICECREAM: return {
+//             ...state,
+//             numOfIceCreams: state.numOfIceCreams - 1
+//         }
+//         default: return state
+//     }
+// }
+
+// Cake Reducer
+const cakeReducer = (state = cakeState, action) => {
     switch (action.type) {
-        case BUY_CAKE: return {
-            ...state,
-            numOfCakes: state.numOfCakes - 1
-        }
-        case BUY_ICECREAM: return {
-            ...state,
-            numOfIceCreams: state.numOfIceCreams - 1
-        }
-        default: return state
+        case 'BUY_CAKE':
+            return {
+                ...state,
+                numOfCakes: state.numOfCakes - 1
+            }
+            break
+        default: state
     }
 }
+
+// IceCream Reducer
+const IceCream = (state = IceCreamState, action) => {
+    switch (action.type) {
+        case 'BUY_ICECREAM':
+            return {
+                ...state,
+                numOfIceCream: state.numOfIceCream - 1
+            }
+            break
+        default: state
+    }
+}
+
 
 const store = createStore(reducer)
 console.log('Initial State', store.getState())
